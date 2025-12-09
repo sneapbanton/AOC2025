eeda51539509b609d7dfdd5c4af5b7a533b49cbc
Anton Eldeborg Lundin <antel460@student.liu.se>
2025-12-09 21:36:23 +0100

    Day 9

--- 9.py
+++ 9.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from tqdm import tqdm
 
 def p1():
     tiles = []
@@ -26,11 +27,17 @@ def p2():
         x,y = map(int,line[:-1].split(",")) if "\n" in line else map(int,line.split(","))
         tiles.append((x,y))
 
+    lines = defaultdict(lambda: False)
     for i in range(len(tiles)):
         x1,y1 = tiles[i-1]
         x2,y2 = tiles[i]
+        for y3 in range(min(y1,y2),max(y1,y2)+1):
+            for x3 in range(min(x1,x2),max(x1,x2)+1):
+                lines[f"{x3},{y3}"] = True
+
 
     areas = defaultdict(lambda:0)
+    areas_l = []
     for (x1,y1) in tiles:
         for (x2,y2) in tiles:
             if (x1==x2 and y1==y2) or (areas[f"{x1},{y1},{x2},{y2}"] or areas[f"{x2},{y2},{x1},{y1}"]):
@@ -38,28 +45,35 @@ def p2():
             dx = abs(x1-x2)+1
             dy = abs(y1-y2)+1
             areas[f"{x1},{y1},{x2},{y2}"] = dx*dy
-    
+            areas_l.append((f"{x1},{y1},{x2},{y2}",dx*dy))
+
+    res = sorted(areas_l,key=lambda x:x[1],reverse=True)
     biggest_area = 0
-    for key in areas:
-        val = areas[key]
+    for key,val in tqdm(res) :
         x1,y1,x2,y2 = map(int,key.split(","))
         if val <= biggest_area:
             continue
         ok = True
         for x3,y3 in tiles:
-            if val == 40:
-                print(x1,y1)
-                print(x2,y2)
-                print(x3,y3)
-                print(min(x1,x2) < x3 < max(x1,x2) and min(y1,y2) < y3 < max(y1,y2))
             if min(x1,x2) < x3 < max(x1,x2) and min(y1,y2) < y3 < max(y1,y2):
                 ok = False
                 break
+        if not ok:
+            continue
+
+        for key in lines:
+            x3,y3 = map(int,key.split(","))
             if min(x1,x2) < x3 < max(x1,x2) and min(y1,y2) < y3 < max(y1,y2):
                 ok = False
                 break
+        if not ok:
+            continue
+        
         if ok:
             biggest_area = val
+            print(x1,y1,"  ",x2,y2)
+            continue
+
 
     print(biggest_area)
 

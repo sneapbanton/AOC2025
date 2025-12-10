1b743fc70f2df8a068e595ac190f2df722ca3e9e
Anton Eldeborg Lundin <antel460@student.liu.se>
2025-12-10 10:35:24 +0100

    Day 10 part 1

--- 10.py
+++ 10.py
@@ -1,32 +1,22 @@
 from tqdm import tqdm
 import sys
 from functools import cache
-sys.setrecursionlimit(2000000000)
-
-def state_dist(goal, state):
-    val = 0
-    for i in range(len(goal)):
-        val = val+1 if goal[i]!=state[i] else val
-    return val
 
 @cache
-def search(goal, state, buttons, lastb):
+def search(goal, state, buttons):
     if all(a==b for a, b in zip(goal, state)):
         return 0
-    distance = state_dist(goal, state)
     min_val = -1
     for i in range(len(buttons)):
         statel = list(state)
-        if i==lastb:
-            continue
         for j in buttons[i]:
             statel[j] = statel[j]==False
-        if state_dist(goal, statel) > distance:
-            continue
+        new_buttons = list(buttons)
+        new_buttons.pop(i)
         if min_val==-1:
-            min_val = 1+search(goal, tuple(state),buttons, j)
+            min_val = 1+search(goal, tuple(statel),tuple(new_buttons))
         else:
-            min_val = min(min_val, 1+search(goal, tuple(state),buttons, j))
+            min_val = min(min_val, 1+search(goal, tuple(statel),tuple(new_buttons)))
     return min_val
 
 def p1():
@@ -49,8 +39,9 @@ def p1():
         for button in buttons:
             b = tuple([int(x) for x in button[1:-1].split(",")])
             bs.append(b)
-        res = search(tuple(g),tuple([False for x in range(len(g))]), tuple(bs), -1)
+        res = search(tuple(g),tuple([False for x in range(len(g))]), tuple(bs))
         score += res
+        # print(res)
 
     print(score)
 p1()
